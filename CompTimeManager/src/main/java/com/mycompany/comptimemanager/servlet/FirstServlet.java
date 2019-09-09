7a57771b812c76e4da4f02cae6578483aa066458
Everton Ferreira <eferreira@viceri.com.br>
2019-09-09 16:14:14 -0300

    first servlet

---
+++ CompTimeManager/src/main/java/com/mycompany/comptimemanager/servlet/FirstServlet.java
@@ -0,0 +1,25 @@
+package com.mycompany.comptimemanager.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet(urlPatterns={"/FirstServlet"})
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+        String nomeEmpresa = request.getParameter("nome");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+	}
+
+}
